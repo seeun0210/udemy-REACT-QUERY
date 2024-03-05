601a180238afdd9d73ff9b3a8177e0d906a4c0ec
goorm <goorm@goormui-MacBookPro.local>
2024-03-05 19:02:55 +0900

    study: 로딩상태와 에러 상태 처리하기

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -12,14 +12,23 @@ export function Posts() {
   // replace with useQuery
   //데이터는 fetchPosts가 결과를 반환한 후에야 정의됨
   //fetchPosts쿼라함수는 비동기 함수로 결과 반환에 시간이 걸림
-  const {data} = useQuery({
+  //isError, isLoading을 추가함으로써 falsy일 때 빈 객체를 반환하지 않고도 로딩중인지 확인 가능함
+  const {data, isError,error,isLoading } = useQuery({
     //쿼리 키: 쿼리 캐시 내의 데이터를 정의
     queryKey: ["posts"],
     //쿼리 함수: 쿼리를 수행하는 함수
     queryFn: fetchPosts,
   });
   //데이터가 falsy 즉 아직 api로 불러 온 값이 없다면,,,
-  if(!data) {return <div/>}
+  //이제 isLoading으로 로딩중인지 탐지 가능
+  if(isLoading) {return <h3>Loading...</h3>}
+  //isLoading vs isFetching
+  //isFetching : 비동기 함수가 아직 해결되지 않았다는 것-> fetch가 완료되지 않았지만,axios호출이나 graphQL호출같은 다른 종류의 데이터를 가져오는 작업일 수 있음
+  //isLoading : isFetching의 하위 집합, 로딩중, 쿼리함수가 아직 미해결이지만 캐싱된 데이터도 없음
+  // 쿼리를 전에 실행한 적이 없어서 데이터를 가져오는 중이고 캐시된 데이터도 없어서 보여줄 수 없음
+  
+  //리액트 쿼리는 default로 쿼리함수를 3번 실행 후에 데이터를 가져올 수 없다고 판단함
+  if(isError){return <h3>Error!!!<p>{error.toString()}</p></h3>}
 
   return (
     <>
