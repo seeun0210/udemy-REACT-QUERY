cce698b3a67a88491d38836f225e90f2e4486f27
goorm <goorm@goormui-MacBookPro.local>
2024-03-05 18:44:55 +0900

    study: useQuery로 쿼리 생성하기

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
@@ -9,7 +10,16 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+  //데이터는 fetchPosts가 결과를 반환한 후에야 정의됨
+  //fetchPosts쿼라함수는 비동기 함수로 결과 반환에 시간이 걸림
+  const {data} = useQuery({
+    //쿼리 키: 쿼리 캐시 내의 데이터를 정의
+    queryKey: ["posts"],
+    //쿼리 함수: 쿼리를 수행하는 함수
+    queryFn: fetchPosts,
+  });
+  //데이터가 falsy 즉 아직 api로 불러 온 값이 없다면,,,
+  if(!data) {return <div/>}
 
   return (
     <>
